58c4b5f2f4eb90ff5de8edb4d25155a6376a9a0c
Damarissss <damaris_dolha@yahoo.com>
2022-11-01 20:08:30 +0200

    Updates: - PaginatedList (with PageIndex and TotalPages); - sorting books by title and price.

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -14,60 +14,68 @@ namespace Dolha_Damaris_Lab2.Data
                 {
                     return; // BD a fost creata anterior (DB has been seeded)
                 }
-                
-                //context.Authors.AddRange(
-                //new Author
-                //{
-                //    FirstName = "Mihail",
-                //    LastName = "Sadoveanu"
-                //},
-                //new Author
-                //{
-                //    FirstName = "George",
-                //    LastName = "Calinescu"
-                //},
-                //new Author
-                //{
-                //    FirstName = "Mircea",
-                //    LastName = "Eliade"
-                //}
-                //);
-                //context.SaveChanges();
 
-                //context.Books.AddRange(
-                //new Book 
-                //{
-                //    Title = "Baltagul", 
-                //    Price = Decimal.Parse("22"),
-                //    AuthorID = context.Authors.Single(author => author.LastName == "Sadoveanu").ID
-                //},
-                //new Book 
-                //{
-                //    Title = "Enigma Otiliei", 
-                //    Price = Decimal.Parse("18"),
-                //    AuthorID = context.Authors.Single(author => author.LastName == "Calinescu").ID
-                //},
-                //new Book
-                //{
-                //    Title = "Maytrei", 
-                //    Price = Decimal.Parse("27"),
-                //    AuthorID = context.Authors.Single(author => author.LastName == "Eliade").ID
-                //}
-                //);
-                //context.SaveChanges();
+                context.Authors.AddRange(
+                new Author
+                {
+                    FirstName = "Mihail",
+                    LastName = "Sadoveanu"
+                },
+                new Author
+                {
+                    FirstName = "George",
+                    LastName = "Calinescu"
+                },
+                new Author
+                {
+                    FirstName = "Mircea",
+                    LastName = "Eliade"
+                }
+                );
+                context.SaveChanges();
+
+                context.Books.AddRange(
+                new Book
+                {
+                    Title = "Baltagul",
+                    Price = Decimal.Parse("22"),
+                    AuthorID = context.Authors.Single(author => author.LastName == "Sadoveanu").ID
+                },
+                new Book
+                {
+                    Title = "Enigma Otiliei",
+                    Price = Decimal.Parse("18"),
+                    AuthorID = context.Authors.Single(author => author.LastName == "Calinescu").ID
+                },
+                new Book
+                {
+                    Title = "Maytrei",
+                    Price = Decimal.Parse("27"),
+                    AuthorID = context.Authors.Single(author => author.LastName == "Eliade").ID
+                }
+                );
+                context.SaveChanges();
 
                 context.Customers.AddRange(
                 new Customer
                 {
                     Name = "Popescu Marcela",
                     Adress = "Str. Plopilor, nr. 24",
-                    BirthDate = DateTime.Parse("1979-09-01")
+                    BirthDate = DateTime.Parse("1979 - 09 - 01")
                 },
                 new Customer
                 {
                     Name = "Mihailescu Cornel",
                     Adress = "Str. Bucuresti, nr. 45, ap. 2",
-                    BirthDate=DateTime.Parse("1969 - 07 - 08")
+                    BirthDate = DateTime.Parse("1969 - 07 - 08")
+                }
+                );
+                context.SaveChanges();
+
+                context.Orders.AddRange(
+                new Order
+                {
+                    OrderDate = DateTime.Parse("2022 - 10 - 31")
                 }
                 );
                 context.SaveChanges();
